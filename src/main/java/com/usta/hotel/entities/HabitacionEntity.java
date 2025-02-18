b31aba280cb04976c4cdb3192d7c628b238a660b
Sofi-06 <mariasofiatm@gmail.com>
2025-02-18 16:21:20 -0500

    Conexion base de datos

--- src/main/java/com/usta/hotel/entities/HabitacionEntity.java
+++ src/main/java/com/usta/hotel/entities/HabitacionEntity.java
@@ -1,4 +1,27 @@
 package com.usta.hotel.entities;
 
-public class Habitacion {
+import jakarta.persistence.*;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "HABITACIONES")
+
+//Serializable: llave primaria autoincrementable
+public class HabitacionEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_habitacion")
+    private Long idHabitacion;
+
+    @NotNull
+    @Size(min=1, max=20)
+    @Column(name="tipo_hab", length = 20, nullable = false)
+    private String tipoHab;
 }
