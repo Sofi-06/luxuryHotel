823ecb2331d049be3580daa9f7af3777ff724d0a
Sofi-06 <mariasofiatm@gmail.com>
2025-02-27 09:57:22 -0500

    Creacion daos

---
+++ src/main/java/com/usta/hotel/models/dao/HabitacionDAO.java
@@ -0,0 +1,21 @@
+package com.usta.hotel.models.dao;
+
+import com.usta.hotel.entities.HabitacionEntity;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+//long-id
+public interface HabitacionDAO extends CrudRepository<HabitacionEntity, Long> {
+
+    @Transactional
+    @Modifying
+    //toma solo un valor que sea igual
+    @Query("UPDATE HabitacionEntity  SET disponibilidad=false  WHERE idHabitacion=?1")
+    public void changeState(Long idHabitacion);
+
+    @Transactional
+    @Query("SELECT HA FROM HabitacionEntity HA WHERE HA.idHabitacion=?1")
+    public HabitacionEntity viewDetail(Long idHabitacion);
+}
---
+++ src/main/java/com/usta/hotel/models/dao/UsuarioDAO.java
@@ -0,0 +1,16 @@
+package com.usta.hotel.models.dao;
+
+import com.usta.hotel.entities.UsuarioEntity;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface UsuarioDAO extends CrudRepository<UsuarioEntity, Long> {
+
+    @Transactional
+    @Modifying
+    //toma solo un valor que sea igual
+    @Query("UPDATE UsuarioEntity  SET estadoUsu=false  WHERE cedula=?1")
+    public void changeState(Long cedula);
+}
